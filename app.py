0560773a16efb16aa64cb156fe2ca6ea0e4709fd
LukeSSJ <41447595+LukeSSJ@users.noreply.github.com>
2022-02-27 13:00:46 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+#Import the flask module
+from flask import Flask
+
+#Create a Flask constructor. It takes name of the current module as the argument
+app = Flask(__name__)
+
+#Create a route decorator to tell the application,
+# which URL should be called for the #described function and define the function
+
+@app.route('/')
+def helloWorld():
+    return "Hello world"
+
+if __name__ == "__main__":
+    app.run(debug=True)
